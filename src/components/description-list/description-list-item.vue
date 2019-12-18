61af77512da1a1867d64cde7be4b79f8c40c6caf
mengxianghan <1056811341@qq.com>
2019-12-18 20:13:44 +0800

    新增`DescriptionList`描述列表组件

---
+++ src/components/description-list/description-list-item.vue
@@ -0,0 +1,39 @@
+<template>
+    <a-col v-bind="span">
+        <div class="term">{{label}}</div>
+        <div class="content"><slot></slot></div>
+    </a-col>
+</template>
+
+<script>
+    const responsive = {
+        1: {xs: 24},
+        2: {xs: 24, sm: 12},
+        3: {xs: 24, sm: 12, md: 8},
+        4: {xs: 24, sm: 12, md: 6}
+    };
+    export default {
+        name: "XDescriptionListItem",
+        props: {
+            label: {
+                type: String,
+                default: '',
+                required: false
+            }
+        },
+        inject: {
+            col: {
+                type: Number
+            }
+        },
+        computed: {
+            span() {
+                return responsive[this.col];
+            }
+        }
+    };
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/system/log/detail-modal.vue
@@ -0,0 +1,33 @@
+<template>
+    <a-modal v-model="visible"
+             :title="title"
+             @ok="onOk"
+             @cancel="onCancel">
+
+    </a-modal>
+</template>
+
+<script>
+    import {form} from '@/utils/mixin';
+
+    export default {
+        mixins: [form],
+        data() {
+            return {};
+        },
+        methods: {
+            handleDetail() {
+                this.title = '详情';
+                this.visible = true;
+            },
+            onOk() {
+            },
+            onCancel() {
+            }
+        }
+    };
+</script>
+
+<style scoped>
+
+</style>
